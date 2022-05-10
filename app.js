333defab9cf3beade286b3fcbca9e556819248b0
Marrwan <Abdulbasitdamilola6@gmail.com>
2022-05-10 11:06:59 +0100

    lool

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(logger('dev'));
 app.use(express.json());
