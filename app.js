5d79f9a3c1d9c92f8f5773b9060f18c36736a76f
Marrwan <Abdulbasitdamilola6@gmail.com>
2022-05-16 02:55:54 +0100

    react production changes

--- app.js
+++ app.js
@@ -31,6 +31,16 @@ app.use(cors(corsOptions))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+if(process.env.NODE_ENV !== 'development'){
+
+  // Step 1:
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  // Step 2:
+  app.get('/verify/:token', auth.verify)
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+}
 
 app.use(logger('dev'));
 app.use(express.json());
